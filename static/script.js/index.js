1a4209f712ed3702ab46d177928ad73c32ef1bb6
KingMagaIII <andreamagaletti14@gmail.com>
2025-02-23 18:25:54 +0100

    stile finito

--- static/script.js/index.js
+++ static/script.js/index.js
@@ -1,9 +1,13 @@
 let button = document.querySelector("#button");
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
     let categoria = document.querySelector("#categories").value
-    let response = fetch("https://api.chucknorris.io/jokes/random?category=" + categoria.toLowerCase())
-        .then(response => response.json())
-        .then(data => {
-            document.querySelector("#quote").innerHTML = data.value
-        })
+    if (categoria == "No category") {
+        document.querySelector("#quote").innerHTML = "No category selected"
+    } else {
+        let response = await fetch("https://api.chucknorris.io/jokes/random?category=" + categoria.toLowerCase())
+            .then(response => response.json())
+            .then(data => {
+                document.querySelector("#quote").innerHTML = data.value
+            })
+    }
 });
